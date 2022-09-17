63cd885fb2a961d54b217e221c10a09cb944c977
Felipe Pontes <107514967+Felipeppontes@users.noreply.github.com>
2022-09-17 17:39:31 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,4 +1,4 @@
-# Projeto-em-python---Media-1-periodo
+# Projeto_python-Media_1periodo
 #Criei uma calculadora de media ponderada, para me auxiliar no 1º periodo da faculdade de engenharia em saber meu CR - coeficiente de rendimento
 
 
